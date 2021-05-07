035be36f23aed3dfec3ff0c2868fa292086b1b7f
iskoti <iskoti@pm.me>
2021-05-07 14:55:43 +0200

    update write test for input.test.js

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,38 +1,33 @@
 import React from 'react';
 import Input from './Input.component';
 import { render, screen, fireEvent } from '@testing-library/react';
-// import {  } from '@testing-library/user-event';
-// import { render, fireEvent, screen } from '@testing-library/react';
-import * as reactRedux from 'react-redux';
-// import inputActions from '../../store/input.slice';
-// import listActions from '../../store/list.slice';
+import { Provider } from 'react-redux';
+import store from '../../store/index';
 
 describe('Input component', () => {
-  const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
-  const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
-
-  beforeEach(() => {
-    useSelectorMock.mockClear();
-    useDispatchMock.mockClear();
-  });
-
   test('has a text input', () => {
-    render(<Input />);
+    render(
+      <Provider store={store}>
+        <Input />
+      </Provider>,
+    );
     const searchInput = screen.getByTitle('searchInput');
     expect(searchInput).toBeInTheDocument();
   });
 
   test('handleChange is called by ...', () => {
-  // const mockSetInputValue = jest.fn();
-  render(<Input />);
-  fireEvent.change(
-    screen.getByPlaceholderText('What would you like to search?'),
-    {
-      target: { value: 'test' },
-    },
-  );
-  const searchInput = screen.getByTitle('searchInput');
-  expect(searchInput.value).toEqual('test');
-  
+    render(
+      <Provider store={store}>
+        <Input />
+      </Provider>,
+    );
+    fireEvent.change(
+      screen.getByPlaceholderText('What would you like to search?'),
+      {
+        target: { value: 'test' },
+      },
+    );
+    const searchInput = screen.getByTitle('searchInput');
+    expect(searchInput.value).toEqual('test');
+  });
 });
-}
